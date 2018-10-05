02bd397d7749beff5f4f9a52f5eb68ec034410be
Ashkan Tirgar <tirgar.ak@gmail.com>
2018-10-04 22:26:53 -0400

    Initial Commit

---
+++ Example/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Example
+//
+//  Created by Ashkan Tirgar on 10/4/18.
+//  Copyright © 2018 Ashkan Tirgar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Example/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Ashkan Tirgar on 10/4/18.
+//  Copyright © 2018 Ashkan Tirgar. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
